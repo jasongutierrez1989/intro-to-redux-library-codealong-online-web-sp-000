ee8f9ec11560c7c767002c8e11a5ea182c74f899
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-23 02:01:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// ./src/index.js
+
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,5 +6,7 @@ import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
+
+const store = createStore(shoppingListItemReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
